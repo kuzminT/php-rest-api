8a7258807017f1d0655703f0d1bfefc2754db910
Timofey Kuzmin <sinnerpocht@gmail.com>
2020-03-04 17:41:36 +0300

    Add fk and relations many to many for photos and announc

--- app/site/app/Announcement.php
+++ app/site/app/Announcement.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Photo;
 
 class Announcement extends Model
 {
     protected $fillable = ['title', 'description'];
+
+    public function photos() {
+        return $this->hasMany(Photo::class);
+    }
 }
--- app/site/app/Photo.php
+++ app/site/app/Photo.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Announcement;
 
 class Photo extends Model
 {
-    protected $fillable = ['url'];
+    protected $fillable = ['url', 'announcement_id'];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function announcement()
+    {
+        return $this->belongsTo(Announcement::class);
+    }
 }
